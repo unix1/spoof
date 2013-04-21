469c14c5d9d25023984eeb2c3a53199f1d984240
Unix1 <unix1@live.com>
2013-04-21 08:38:59 -0700

    add initial set of classes for php erlang bridge

---
+++ lib360/net/erlang/peb/Exception.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace lib360\net\erlang\peb;
+
+class Exception extends \Exception
+{
+}
+
+?>
---
+++ lib360/net/erlang/peb/value/IValue.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace lib360\net\erlang\peb\value;
+
+interface IValue
+{
+
+	public function __construct($value, $type);
+
+}
+
+?>
