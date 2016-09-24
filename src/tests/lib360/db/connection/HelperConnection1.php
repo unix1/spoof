d707d38629540e22d5cf06d14feaacf30fac85c0
Zurab <unix1@live.com>
2016-09-23 22:31:08 -0700

    Update comment style of license header

--- src/tests/lib360/db/connection/HelperConnection1.php
+++ src/tests/lib360/db/connection/HelperConnection1.php
@@ -2,22 +2,22 @@
 
 namespace spoof\tests\lib360\db\connection;
 
-/*
-    This is Spoof.
-    Copyright (C) 2011-2012  Spoof project.
-
-    Spoof is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Spoof is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Spoof.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ *  This is Spoof.
+ *  Copyright (C) 2011-2012  Spoof project.
+ *
+ *  Spoof is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Spoof is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Spoof.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 class HelperConnection1 extends \spoof\lib360\db\connection\Connection
