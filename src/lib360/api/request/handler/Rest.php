a8e42272fec6a9ca0a1a6ce77bd65fa6a0a7e7cf
Zurab <unix1@live.com>
2017-07-29 10:44:45 -0700

    Add missing response statuses in Rest request handler

--- src/lib360/api/request/handler/Rest.php
+++ src/lib360/api/request/handler/Rest.php
@@ -35,8 +35,10 @@ class Rest extends api\request\Handler
         api\Response::STATUS_OK => '200 OK',
         api\Response::STATUS_OK_CREATED => '201 Created',
         api\Response::STATUS_BAD_REQUEST => '400 Bad Request',
+        api\Response::STATUS_AUTH_FAILED => '401 Unauthorized',
         api\Response::STATUS_RESOURCE_NOT_FOUND => '404 Not Found',
         api\Response::STATUS_ERROR => '500 Internal Server Error',
+        api\Response::STATUS_METHOD_NOT_DEFINED => '501 Not Implemented',
         api\Response::STATUS_METHOD_NOT_IMPLEMENTED => '501 Not Implemented',
     );
 
